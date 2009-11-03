4e273a0f96535314862f942256b41552b981ffc1
Chris Sessions <sessions@flying-saucer.net>
2009-11-03 11:33:27 -0800

    Cleanup

--- rails/init.rb
+++
@@ -1,5 +0,0 @@
-require 'jitterbug'
-
-# ActionController::Base.extend Jitterbug::ControllerMixin
-
-RAILS_DEFAULT_LOGGER.info("** Jitterbug initialized with these settings:\n#{Jitterbug::Config.settings.to_yaml}") if defined?(RAILS_DEFAULT_LOGGER)
